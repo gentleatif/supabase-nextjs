90bb47ccf73a42003cb45aa127ad11a9c8350767
coderatif@gmail.com <coderatif@gmail.com>
2025-02-25 10:30:51 +0530

    updated config for image optmization fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,19 @@
 const Footer = () => {
   return (
-    <footer className="bg-transparent text-white py-4">
-      <div className="container mx-auto px-4 text-center text-sm">
-        Task App by Pixegami 🛠️ View on{" "}
+    <footer className='bg-transparent text-white py-4'>
+      <div className='container mx-auto px-4 text-center text-sm'>
+        Task App by Atif{' '}
         <a
-          href="https://github.com/pixegami/task-app-project"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="underline"
+          href='https://github.com/gentleatif/supabase-nextjs.git'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='underline'
         >
           GitHub
         </a>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,16 @@
 const nextConfig = {
   distDir: 'build',
   output: 'export',
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        // This becomes "[your-project-id].supabase.co"
-        hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname,
-        pathname: '**'
-      }
-    ]
-  }
+  images: { unoptimized: true }
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: new URL(process.env.NEXT_PUBLIC_SUPABASE_URL).hostname,
+  //       pathname: '**'
+  //     }
+  //   ]
+  // }
 }
 
 export default nextConfig
